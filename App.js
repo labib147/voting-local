38c654262d43b30c30c0fd604fe3346c0aa23797
Nefaur Rahman Labib <52388164+labib147@users.noreply.github.com>
2022-08-10 13:21:22 +0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,62 @@
+//import logo from './logo.svg';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import './App.css';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+
+function App() {
+  const [greeting, setGreetingValue] = useState() 
+
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });    
+  }
+
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      console.log({ provider })
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()
+        console.log('data: ', data)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }
+  }
+
+  async function setGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(tokenAddress, Token.abi, provider)
+      const balance = await contract.balanceOf(account);
+      console.log("Balance: ", balance.toString());
+    }
+  }
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
